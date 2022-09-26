9598dfd051a2c5a3191af96407925a30e4781a65
Tousif Habib <thabib@mun.ca>
2022-09-25 22:12:18 -0230

    Running Linter

--- app/controllers/api/survivor_controller.rb
+++ app/controllers/api/survivor_controller.rb
@@ -1,175 +1,179 @@
-class Api::SurvivorController < ApplicationController
+# frozen_string_literal: true
 
+module Api
+  class SurvivorController < ApplicationController
     # GET /api/survivor
     def index
-        @survivors = Survivor.all
-        render json: { status: 'SUCCESS', message: 'List of survivors', data:@survivors}, status: :created
+      @survivors = Survivor.all
+      render json: { status: 'SUCCESS', message: 'List of survivors', data: @survivors }, status: :created
     end
 
     # POST /api/survivor
     def create
-        @survivor = Survivor.new(survivor_params)
-        if @survivor.save
-            render json: @survivor
-        else
-            render error: { error: 'Unable to create a Survivor' }, status: 400
-        end
+      @survivor = Survivor.new(survivor_params)
+      if @survivor.save
+        render json: @survivor
+      else
+        render error: { error: 'Unable to create a Survivor' }, status: 400
+      end
     end
 
     # PUT /api/survivor/:id/location
     def location
-        @survivor = Survivor.find(params[:id])
-        if @survivor.update(survivor_location_params)
-            render json: { status: 'SUCCESS', message: 'Updated survivor location', name:@survivor[:name] , latitude: @survivor[:latitude], longitude: @survivor[:longitude] }, status: :ok
-        else
-            render json: { status: 'ERROR', message: "Unable to update Survivor location", data:@survivor.errors }, status: :unprocessable_entity
-        end
+      @survivor = Survivor.find(params[:id])
+      if @survivor.update(survivor_location_params)
+        render json: { status: 'SUCCESS', message: 'Updated survivor location', name: @survivor[:name], latitude: @survivor[:latitude], longitude: @survivor[:longitude] },
+               status: :ok
+      else
+        render json: { status: 'ERROR', message: 'Unable to update Survivor location', data: @survivor.errors },
+               status: :unprocessable_entity
+      end
     end
 
     # PUT /api/survivor/:id/infected
     def infected
-        @survivor = Survivor.find(params[:id])
-        
-        numInfections = @survivor[:infectionCount]
-        numInfections += 1
+      @survivor = Survivor.find(params[:id])
 
-        if @survivor.update(:infectionCount => numInfections)
-            if numInfections >= 5
-                @survivor.update(:infected => true)
-                render json: { status: 'SUCCESS', message: 'Updated survivor infection status, survivor is infected', name:@survivor[:name] , infectionCount: @survivor[:infectionCount], infected: @survivor[:infected] }, status: :ok
-            else
-                render json: { status: 'SUCCESS', message: 'Updated survivor infection status, survivor is not infected', name:@survivor[:name] , infectionCount: @survivor[:infectionCount], infected: @survivor[:infected] }, status: :ok
-            end
+      numInfections = @survivor[:infectionCount]
+      numInfections += 1
+
+      if @survivor.update(infectionCount: numInfections)
+        if numInfections >= 5
+          @survivor.update(infected: true)
+          render json: { status: 'SUCCESS', message: 'Updated survivor infection status, survivor is infected', name: @survivor[:name], infectionCount: @survivor[:infectionCount], infected: @survivor[:infected] },
+                 status: :ok
         else
-            render json: { status: 'ERROR', message: "Unable to update survivor infection status", data:@survivor.errors }, status: :unprocessable_entity
+          render json: { status: 'SUCCESS', message: 'Updated survivor infection status, survivor is not infected', name: @survivor[:name], infectionCount: @survivor[:infectionCount], infected: @survivor[:infected] },
+                 status: :ok
         end
+      else
+        render json: { status: 'ERROR', message: 'Unable to update survivor infection status', data: @survivor.errors },
+               status: :unprocessable_entity
+      end
     end
 
     # POST /api/survivor/trade
     def trade
-        # This hash has the number of points that each item is worth
-        tradeValues = {
-            "water" => 14,
-            "soup" => 12,
-            "firstAid" => 10,
-            "ak47" => 8
-        }
+      # This hash has the number of points that each item is worth
+      tradeValues = {
+        'water' => 14,
+        'soup' => 12,
+        'firstAid' => 10,
+        'ak47' => 8
+      }
 
-        buyer = Survivor.find(survivor_trade_params[:buyerId])
-        seller = Survivor.find(survivor_trade_params[:sellerId])
+      buyer = Survivor.find(survivor_trade_params[:buyerId])
+      seller = Survivor.find(survivor_trade_params[:sellerId])
 
-        # Check if either the buyer or seller is infected
-        if isInfected(buyer[:infected], seller[:infected]) == false
-            # Check if buyer and seller has items in their inventory to trade and checks if the number of items to be traded is valid
-           if hasItems(buyer, seller, survivor_trade_params[:itemsToBuy], survivor_trade_params[:itemsToSell]) == true
-            if 
-                validTrade(buyer, seller, survivor_trade_params[:itemsToBuy], survivor_trade_params[:itemsToSell], tradeValues)
-            end
-           else
-            puts "FALSE"
-           end
-
+      # Check if either the buyer or seller is infected
+      if isInfected(buyer[:infected], seller[:infected]) == false
+        # Check if buyer and seller has items in their inventory to trade and checks if the number of items to be traded is valid
+        if hasItems(buyer, seller, survivor_trade_params[:itemsToBuy], survivor_trade_params[:itemsToSell]) == true
         else
-            puts "IMPLEMENT REJECTION LOGIC"
+          puts 'FALSE'
         end
 
+      else
+        puts 'IMPLEMENT REJECTION LOGIC'
+      end
     end
 
     private
-    
+
     def survivor_params
-        params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude, :water, :soup, :firstAid, :ak47, :infected)
+      params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude, :water, :soup, :firstAid, :ak47,
+                                       :infected)
     end
-    
+
     def survivor_location_params
-        params.require(:location).permit(:latitude, :longitude)
+      params.require(:location).permit(:latitude, :longitude)
     end
 
     def survivor_trade_params
-        params.require(:trade).permit(
-            :buyerId, 
-            :sellerId, 
-            itemsToBuy: [:water, :soup, :firstAid, :ak47], 
-            itemsToSell: [:water, :soup, :firstAid, :ak47])
+      params.require(:trade).permit(
+        :buyerId,
+        :sellerId,
+        itemsToBuy: %i[water soup firstAid ak47],
+        itemsToSell: %i[water soup firstAid ak47]
+      )
     end
 
     def isInfected(buyerInfectionStatus, sellerInfectionStatus)
-        if buyerInfectionStatus == true || sellerInfectionStatus == true
-            return true
-        else
-            return false
-        end
+      if buyerInfectionStatus == true || sellerInfectionStatus == true
+        true
+      else
+        false
+      end
     end
 
     def hasItems(buyer, seller, itemsToBuy, itemsToSell)
-        # Check if buyer has items to sell
-        buyerItemsToSell = buyer.slice(itemsToSell.keys)
-        sellerItemsToBuy = seller.slice(itemsToBuy.keys)
+      # Check if buyer has items to sell
+      buyerItemsToSell = buyer.slice(itemsToSell.keys)
+      sellerItemsToBuy = seller.slice(itemsToBuy.keys)
 
-        # puts "REMOVE-ME #{buyerItemsToSell}"
-        # puts "REMOVE-ME #{sellerItemsToBuy}"
-        # puts "REMOVE-ME #{itemsToBuy}"
-        # puts "REMOVE-ME #{itemsToSell}"
+      # puts "REMOVE-ME #{buyerItemsToSell}"
+      # puts "REMOVE-ME #{sellerItemsToBuy}"
+      # puts "REMOVE-ME #{itemsToBuy}"
+      # puts "REMOVE-ME #{itemsToSell}"
 
-        # Check if the correct number of items the buyer wants to sell exists in their inventory
-        buyerItemsToSell.values.each_index do |i|
-            if buyerItemsToSell.values[i] < itemsToSell.values[i]
-                # puts "REMOVE-ME FALSCH"
-                return false
-            end
+      # Check if the correct number of items the buyer wants to sell exists in their inventory
+      buyerItemsToSell.values.each_index do |i|
+        if buyerItemsToSell.values[i] < itemsToSell.values[i]
+          # puts "REMOVE-ME FALSCH"
+          return false
         end
+      end
 
-        # Check if the correct number of items the seller wants to buy exists in their inventory
-        sellerItemsToBuy.values.each_index do |i|
-            if sellerItemsToBuy.values[i] < itemsToBuy.values[i]
-                # puts "REMOVE-ME FALSCH"
-                return false
-            end
+      # Check if the correct number of items the seller wants to buy exists in their inventory
+      sellerItemsToBuy.values.each_index do |i|
+        if sellerItemsToBuy.values[i] < itemsToBuy.values[i]
+          # puts "REMOVE-ME FALSCH"
+          return false
         end
+      end
 
-        return true
+      true
     end
 
     def validTrade(buyer, seller, itemsToBuy, itemsToSell, tradeValues)
-        buyerItemsToSell = buyer.slice(itemsToSell.keys)
-        sellerItemsToBuy = seller.slice(itemsToBuy.keys)
+      buyerItemsToSell = buyer.slice(itemsToSell.keys)
+      sellerItemsToBuy = seller.slice(itemsToBuy.keys)
 
-        # Only items that the buyer and seller respectively have will be extracted from the tradeValues hash
-        buyerTradeValues = tradeValues.select { |key, value| itemsToSell.include? key }
-        sellerTradeValues = tradeValues.select { |key, value| itemsToBuy.include? key }
+      # Only items that the buyer and seller respectively have will be extracted from the tradeValues hash
+      buyerTradeValues = tradeValues.select { |key, _value| itemsToSell.include? key }
+      sellerTradeValues = tradeValues.select { |key, _value| itemsToBuy.include? key }
 
-        # Calculate aggregate points of buyer
-        buyerSum = 0
-        sellerSum = 0
+      # Calculate aggregate points of buyer
+      buyerSum = 0
+      sellerSum = 0
 
-        # Calculates aggregate points of what the buyer is offering
-        buyerTradeValues.values.each_index do |i|
-            buyerSum += buyerTradeValues.values[i] * itemsToSell.values[i]
-        end
+      # Calculates aggregate points of what the buyer is offering
+      buyerTradeValues.values.each_index do |i|
+        buyerSum += buyerTradeValues.values[i] * itemsToSell.values[i]
+      end
 
-        # Calculates aggregate points of what the seller is offering
-        sellerTradeValues.values.each_index do |i|
-            sellerSum += sellerTradeValues.values[i] * itemsToBuy.values[i]
-        end
+      # Calculates aggregate points of what the seller is offering
+      sellerTradeValues.values.each_index do |i|
+        sellerSum += sellerTradeValues.values[i] * itemsToBuy.values[i]
+      end
 
-        # If the aggregate points for buyer and seller match, the DB record will be updated
-        if buyerSum == sellerSum
-            
-            puts "HELLO!"
-        else
-            puts "NO!"
-            return false
-        end
+      # If the aggregate points for buyer and seller match, the DB record will be updated
+      if buyerSum == sellerSum
 
-        puts "REMOVE-ME #{buyerSum}"
-        puts "REMOVE-ME #{sellerSum}"
+        puts 'HELLO!'
+      else
+        puts 'NO!'
+        return false
+      end
 
-        puts "REMOVE-ME #{tradeValues}"
-        puts "REMOVE-ME buyer trade values#{buyerTradeValues}"
-        puts "REMOVE-ME buyer items to sell#{buyerItemsToSell}"
-        puts "REMOVE-ME seller trade values#{sellerTradeValues}"
-        puts "REMOVE-ME seller itesm to sell#{sellerItemsToBuy}"
+      puts "REMOVE-ME #{buyerSum}"
+      puts "REMOVE-ME #{sellerSum}"
 
+      puts "REMOVE-ME #{tradeValues}"
+      puts "REMOVE-ME buyer trade values#{buyerTradeValues}"
+      puts "REMOVE-ME buyer items to sell#{buyerItemsToSell}"
+      puts "REMOVE-ME seller trade values#{sellerTradeValues}"
+      puts "REMOVE-ME seller itesm to sell#{sellerItemsToBuy}"
     end
-
+  end
 end
--- db/migrate/20220924180723_create_survivors.rb
+++ db/migrate/20220924180723_create_survivors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateSurvivors < ActiveRecord::Migration[7.0]
   def change
     create_table :survivors do |t|
@@ -6,12 +8,12 @@ class CreateSurvivors < ActiveRecord::Migration[7.0]
       t.string :gender, null: false
       t.float :latitude, null: false
       t.float :longitude, null: false
-      t.integer :water, :default => 0, null: true
-      t.integer :soup, :default => 0, null: true
-      t.integer :firstAid, :default => 0, null: true
-      t.integer :ak47, :default => 0, null: true
-      t.integer :infectionCount, :default => 0, null: false
-      t.boolean :infected, :default => false, null: false
+      t.integer :water, default: 0, null: true
+      t.integer :soup, default: 0, null: true
+      t.integer :firstAid, default: 0, null: true
+      t.integer :ak47, default: 0, null: true
+      t.integer :infectionCount, default: 0, null: false
+      t.boolean :infected, default: false, null: false
 
       t.timestamps
     end
